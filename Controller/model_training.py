d230b554efe192bc4cd6eaf833ad7d5e998a4d31
Farhan Momin <farhan.m.nafis2412@gmail.com>
2024-04-14 21:12:27 +1000

    Minor changes to Model Training

--- Controller/model_training.py
+++ Controller/model_training.py
@@ -2,13 +2,9 @@ import argparse
 
 
 def train_model(preprocessed_dataset_id, split_ratio, project_name, queue_name):
-    from clearml import Task, Dataset, OutputModel
-
-    task = Task.init(project_name=project_name, task_name="Model Training")
-    task.execute_remotely(queue_name=queue_name, exit_process=True)
-
     import os
     import matplotlib.pyplot as plt
+    from clearml import Task, Dataset, OutputModel
     import tensorflow as tf
     from tensorflow.keras.preprocessing.image import ImageDataGenerator
     from tensorflow.keras.applications import ResNet50V2
@@ -16,6 +12,9 @@ def train_model(preprocessed_dataset_id, split_ratio, project_name, queue_name):
     from tensorflow.keras.models import Model
     from tensorflow.keras.optimizers import Adam
     from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+    task = Task.init(project_name=project_name, task_name="Model Training")
+    task.execute_remotely(queue_name=queue_name, exit_process=True)
 
     # Load preprocessed dataset
     dataset = Dataset.get(dataset_id=preprocessed_dataset_id)
